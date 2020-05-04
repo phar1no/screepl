9647365eaeeaa0e7841dadfa76aad65b45370669
phar1no <i.komyagin@gmail.com>
2020-05-04 19:03:04 +0300

    added script

---
+++ screepl.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# 
+# Share an area of the screen
+# Tested on Ubuntu 19.10
+#
+# Reqs:
+#  gnome-screenshot
+#  ffsend
+#  notify-send
+#  zenity
+#
+# https://github.com/phar1no/screepl
+#
+
+SCREEN_DIR=/home/qom/Pictures/Screens2Upload/
+date_name=$(date +"%Y-%m-%d_%H:%M:%S")
+file_name=${SCREEN_DIR}Screeshot_${date_name}.png
+
+gnome-screenshot -af ${file_name}
+
+#DEBUG
+#echo "$?"
+#echo `ls -l ${file_name}`
+
+if [ $? == 0 ] && [ -e ${file_name} ]
+then
+	notify-send 'Screenshot saved to' $file_name
+	short_link=$(ffsend upload -Syc $file_name 2> /dev/null) 
+	notify-send 'Screenshot uploaded to' $short_link 
+	exit 0
+
+else
+	#notify-send 'Screenshot' 'Error'
+	zenity --warning --text "Screenshot Error"
+	exit 1
+fi
